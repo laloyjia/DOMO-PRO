70e398d38b26ad2aa3e19a3239f372c27e24c9d2
laloeayj <lalo.yj.ia@email.com>
2026-03-03 15:21:26 -0300

    fix: force automatic jsx runtime in vite config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  // Añade esta parte para forzar el runtime automático
-  esbuild: {
-    jsxInject: `import React from 'react'`
+  plugins: [
+    react({
+      // Esto asegura que Vite maneje el JSX sin errores de "React is not defined"
+      jsxRuntime: 'automatic', 
+    })
+  ],
+  build: {
+    outDir: 'dist',
   }
 })
